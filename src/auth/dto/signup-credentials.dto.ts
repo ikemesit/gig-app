26ef5c88e3d4dac2ac62d336db8d0503fabad75d
Ikemesit Etuknwa <ikemesit@gmail.com>
2019-08-18 23:35:18 +0800

    Add validation to signup payload

--- src/auth/dto/signup-credentials.dto.ts
+++ src/auth/dto/signup-credentials.dto.ts
@@ -1,30 +1,53 @@
-import { IsNotEmpty } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
 
 export class SignupCredentialsDto {
+  @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @IsString()
   @IsNotEmpty()
   lastName: string;
 
+  @IsString()
   @IsNotEmpty()
   middleName: string;
 
+  @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @IsString()
   @IsNotEmpty()
   phone: string;
 
+  @IsString()
   @IsNotEmpty()
   address: string;
 
+  @IsString()
   @IsNotEmpty()
   dob: string;
 
+  @MinLength(4)
+  @MaxLength(20)
+  @IsString()
   @IsNotEmpty()
   username: string;
 
+  @MinLength(4)
+  @MaxLength(20)
+  @IsString()
   @IsNotEmpty()
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Password is too weak',
+  })
   password: string;
 }
